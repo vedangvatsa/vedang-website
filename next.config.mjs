42db0d00af37704f614e813f4c72a7af878817b5
vedangvatsa123 <vedangvats@gmail.com>
2026-03-24 03:45:11 +0530

    fix: Add font-src 'self' data: to CSP to unblock data URI fonts

--- next.config.mjs
+++ next.config.mjs
@@ -65,7 +65,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://platform.twitter.com https://cdn.syndication.twimg.com; img-src 'self' data: https:; style-src 'self' 'unsafe-inline'; frame-src https://www.youtube.com https://platform.twitter.com https://syndication.twitter.com; connect-src 'self' https://www.google-analytics.com https://syndication.twitter.com https://cdn.syndication.twimg.com;",
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://platform.twitter.com https://cdn.syndication.twimg.com; img-src 'self' data: https:; style-src 'self' 'unsafe-inline'; font-src 'self' data:; frame-src https://www.youtube.com https://platform.twitter.com https://syndication.twitter.com; connect-src 'self' https://www.google-analytics.com https://syndication.twitter.com https://cdn.syndication.twimg.com;",
           },
           {
             key: 'X-Robots-Tag',
